150a4dcf1b2dae99071f04d025218d5227aad280
nyarurato <nyarurato202@gmail.com>
2023-03-25 13:58:09 +0900

    Add Debug Menu

---
+++ wwwroot/script/functions.js
@@ -0,0 +1,11 @@
+﻿function getBrowserLocale() {
+    return (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
+}
+window.blazorCulture = {
+    get: () => window.localStorage['BlazorCulture'],
+    set: (value) => window.localStorage['BlazorCulture'] = value
+};
+
+function scrollTextareaToBottom(textarea) {
+    textarea.scrollTop = textarea.scrollHeight;
+}
